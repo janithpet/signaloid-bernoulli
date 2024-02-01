c50dce3464b8613f85de2a1811ca2de968905aad
janithPet <jpetangoda@gmail.com>
2024-02-01 23:29:33 +0530

    removed unused uxhw functions from local implementation

--- local-include/uxhw.h
+++ local-include/uxhw.h
@@ -4,17 +4,9 @@
 #include <stddef.h>
 
 /**
- * @brief Simulates the <a href="https://docs.signaloid.io/docs/hardware-api/inserting-uncertainty/dist-from-samples/">UxHwDoubleDistFromSamples from the Signaloid UxHw Hardware API</a>.
- *
- * @param samples An array of samples from the underlying distribution.
- * @param sampleCount The number of samples. I.e the length of the `samples` array .
- *
- * @return Currently returns average of the samples.
- */
-double UxHwDoubleDistFromSamples(double* samples, size_t sampleCount);
-
-/**
- * @brief Simulates the <a href="https://docs.signaloid.io/docs/hardware-api/inserting-uncertainty/parametric-distributions/uniform-dist/">UxHwDoubleUniformDist from the Signaloid UxHw Hardware API</a>.
+ * @brief Simulates the <a
+ * href="https://docs.signaloid.io/docs/hardware-api/inserting-uncertainty/parametric-distributions/uniform-dist/">UxHwDoubleUniformDist
+ * from the Signaloid UxHw Hardware API</a>.
  *
  * @param min The minimum value of the distribution.
  * @param max The maximum value of the distribution.
@@ -22,15 +14,4 @@ double UxHwDoubleDistFromSamples(double* samples, size_t sampleCount);
  * @return The average of the min and max.
  */
 double UxHwDoubleUniformDist(double min, double max);
-
-/**
- * @brief Simulates the <a href="https://docs.signaloid.io/docs/hardware-api/inserting-uncertainty/parametric-distributions/gauss-dist/">UxHwDoubleGaussDist from the Signaloid UxHw Hardware API</a>.
- *
- * @param mean The mean of the distribution.
- * @param stddev The standard deviation of the distribution.
- *
- * @return The mean.
- */
-double UxHwDoubleGaussDist(double mean, double stddev);
-
 #endif
--- src/uxhw.c
+++ src/uxhw.c
@@ -5,18 +5,4 @@
 #include <stdlib.h>
 #include <time.h>
 
-double UxHwDoubleDistFromSamples(double* samples, size_t samplesCount) {
-	double sum = 0;
-	for (int i = 0; i < samplesCount; i++) {
-		sum += samples[i];
-	}
-	return sum / samplesCount;
-}
-
-double UxHwDoubleUniformDist(double min, double max) {
-	return (max + min) / 2;
-}
-
-double UxHwDoubleGaussDist(double mean, double stddev) {
-	return mean;
-}
+double UxHwDoubleUniformDist(double min, double max) { return (max + min) / 2; }
