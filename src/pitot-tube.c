997afd94fe3f4efb38b7f46800a0f28c2cce764c
janithPet <jpetangoda@gmail.com>
2024-02-02 00:08:29 +0530

    testing why negative in signaloid distribution

--- src/pitot-tube.c
+++ src/pitot-tube.c
@@ -9,8 +9,10 @@ const double PITOT_TUBE_ERROR_FULL_SCALE = 0.5 / 100; // of full scale
 
 double pitot_tube(double true_pressure_difference, double fluid_density) {
 	// This function add uniform noises equal to \f$\pm 0.5\%\f$ of full scale to the true pressure difference.
-	double error          = PITOT_TUBE_ERROR_FULL_SCALE * (PITOT_TUBE_RANGE_MAX - PITOT_TUBE_RANGE_MIN);
-	double exact_velocity = sqrt((2 * true_pressure_difference) / fluid_density);
+	double error = PITOT_TUBE_ERROR_FULL_SCALE * (PITOT_TUBE_RANGE_MAX - PITOT_TUBE_RANGE_MIN);
 
-	return UxHwDoubleUniformDist(exact_velocity - error, exact_velocity + error);
+	double uncertain_pressure_difference =
+			UxHwDoubleUniformDist(true_pressure_difference - error, true_pressure_difference + error);
+
+	return sqrt((2 * uncertain_pressure_difference) / fluid_density);
 }
